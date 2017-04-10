4cbeba32c5a5caca1871dea23bb310fd2db06bcf
yudawei1230 <374149489@qq.com>
2017-04-10 22:56:51 +0800

    'test2'

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const exec = require('child_process').exec;
-exec('update.bat', (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('\033[2J');
-  console.log(stdout);
-});
+setTimeout(function(){
+	exec('update.bat', (err, stdout, stderr) => {
+	  if (err) {
+	    console.error(err);
+	    return;
+	  }
+	  console.log('\033[2J');　
+	  console.log(stdout);
+	});
+　
+},3000)
